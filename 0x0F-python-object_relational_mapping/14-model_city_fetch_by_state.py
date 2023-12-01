9defe8bc12b8919c1d029dfaa6809c64425a1ad8
AdetunjiDanielOlamide <adetunjidanielolamide@gmail.com>
2023-11-30 18:05:59 -0800

    done

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""""
-A script that deletes all State objects with a name containin
-the letter a from the database hbtn_0e_6_usa
+"""
+prints all City objects from a database
 """
 
-
-from sys import argv
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sys import argv
+from model_state import Base, State
+from model_city import City
 
 
-if __name__ == '__main__':
-    dbEngine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=dbEngine)
-    session = InstanceSession()
-
-    states = session.query(State).filter(State.name.contains('a')).all()
-
-    for state in states:
-        session.delete(state)
-
-    session.commit()
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    rows = session.query(City, State).filter(City.state_id == State.id)\
+                                     .order_by(City.id).all()
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
